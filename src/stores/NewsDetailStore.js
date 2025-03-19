f314e5773185f4f063fb729023a7d4e2c63b327e
Sergey Koveshnikov <sergey.kove@mail.ru>
2025-03-19 12:08:41 +0300

    add

--- src/stores/NewsDetailStore.js
+++ src/stores/NewsDetailStore.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 export const useNewsDetailStore = defineStore('getValue', () => {
   const detailNews = ref([])
-  async function getNews(val) {
-    const data = await axios.get(val)
+  async function getNews(url) {
+    const data = await axios.get(url)
     detailNews.value = data.data.data.news
   }
 
