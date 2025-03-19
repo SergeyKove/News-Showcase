815daaa05170ca39bc24e2bbb2989f9f0f478769
Sergey Koveshnikov <sergey.kove@mail.ru>
2025-03-19 12:08:29 +0300

    add

--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -6,6 +6,10 @@
           <div class="flex">
             <NewsBlock v-for="news in cardNews" :news="news" />
           </div>
+          <div class="q-pa-lg flex flex-center">
+            <q-pagination v-model="page" :max="pageCount" @update:model-value="getNews">
+            </q-pagination>
+          </div>
         </div>
       </q-page>
     </q-page-container>
@@ -15,12 +19,20 @@
 <script setup>
 import NewsBlock from '@/components/NewsBlock.vue'
 import { useNewsStore } from '@/stores/NewsStore'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const store = useNewsStore()
+const page = ref(1)
 
-store.getNews()
 const cardNews = computed(() => {
   return store.cardNews
 })
+const pageCount = computed(() => {
+  return store.pageCount
+})
+
+const getNews = function () {
+  store.getNews(page.value)
+}
+getNews()
 </script>
--- src/stores/NewsStore.js
+++ src/stores/NewsStore.js
@@ -2,17 +2,20 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import axios from 'axios'
 
-const url = 'https://domotekhnika.ru/api/v1/news?page=7'
+const url = 'https://domotekhnika.ru/api/v1/news'
 
 export const useNewsStore = defineStore('getNews', () => {
   const cardNews = ref([])
-  async function getNews() {
-    const data = await axios.get(url)
+  const pageCount = ref('')
+  async function getNews(pageNumber) {
+    const data = await axios.get(url, { params: { page: pageNumber } })
     cardNews.value = data.data.data.news
+    pageCount.value = data.data.data.pageCount
   }
 
   return {
     cardNews,
+    pageCount,
     getNews,
   }
 })
